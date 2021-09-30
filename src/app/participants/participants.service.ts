680ea8fae77e3f502f0ce98523ac078c8d0025bf
jamping882 <jamppalapar@gmail.com>
2021-09-30 11:04:34 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Feliciana Poquita", age:79 + " years old", gender: "Female"},
+            {name: "Juan Ponce Enrile", age:96 + " years old", gender: "Male"},
+            {name: "Junie Makaburot", age:32 + " years old", gender: "Male"},
+            {name: "Ben Tennyson", age:37 + " years old", gender: "Male"},
+            {name: "Dhain Karl Taguines", age:61 + " years old", gender: "Male"},
+            {name: "Hipolito Kardo", age:38 + " years old", gender: "Male"},
+            {name: "Christian Noel Asucan", age:24 + " years old", gender: "Male"},
+            {name: "Beer Young", age:67 + " years old", gender: "Female"},
+        ];
+    }
+
+}
